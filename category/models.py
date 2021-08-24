970b73518a1112268d4dd08fb0707e67c0e5016b
Stephen Suyanka <stepkans@gmail.com>
2021-08-24 22:26:03 +0300

    This is my first commit

---
+++ category/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models.aggregates import Max
+from django.db.models.fields.files import ImageField
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories')
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])    
+        
+    def __str__(self):
+        return self.category_name
