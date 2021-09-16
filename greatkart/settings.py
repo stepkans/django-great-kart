7893085eb76ba9796ac79ceff6cdf5f0e752e88e
Stephen Suyanka <stepkans@gmail.com>
2021-09-16 15:50:08 +0300

    Added django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,11 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
